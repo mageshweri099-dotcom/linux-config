895cc911746d90e8ebf698e40cf7b0b4495689c3
hellopradeep69 <hellopradeep69@gmail.com>
2026-02-09 12:30:23 +0530

    distro named

--- setup.sh
+++ setup.sh
@@ -4,6 +4,7 @@ echo "Vicky Fuck Off Start"
 
 Install_app(){
     if command -v zypper >/dev/null 2>&1;then
+        echo "Installing package in Opensuse"
         sudo zypper install firefox vscode git gh-cli trash-cli zsh btop
     fi
 }
