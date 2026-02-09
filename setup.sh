d41f1fde135dc4fe05371ba94c670d8c56f4a1ba
hellopradeep69 <hellopradeep69@gmail.com>
2026-02-09 23:29:15 +0530

    feat(arch): arch bein added

--- setup.sh
+++ setup.sh
@@ -5,9 +5,13 @@ echo "Vicky Fuck Off Start"
 Install_app(){
     if command -v zypper >/dev/null 2>&1;then
         echo "Installing package in Opensuse"
-        sudo zypper install firefox vscode git gh-cli trash-cli zsh btop ghostty
+        sudo zypper install firefox vscode git trash-cli zsh btop ghostty github-cli
     elif command -v pkg >/dev/null 2>&1;then
         # TODO: for freebsd
+    elif command -v pacman >/dev/null 2>&1;then
+        echo "Installing package in Arch"
+        sudo pacman -S --noconfirm --needed firefox vscode git gh-cli \
+            trash-cli zsh btop ghostty curl wget
     fi
 }
 
